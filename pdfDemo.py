801bdfe5c080e347ce90a547619e8f57f24cf742
luckysher <luckyshera95@gmail.com>
2018-05-31 00:07:04 +0530

    added class for generating pdf file

---
+++ pdfDemo.py
@@ -0,0 +1,11 @@
+###################################################
+#                                                 #
+#    Class for generating demo PDF file           #
+#                                                 #
+###################################################
+
+
+class PDFDemo:
+    """
+    Class for generating PDF file with additional graphical data
+    """
