8502d77acf93674d7da920f01a2c3b406cae8ab1
luckysher <luckyshera95@gmail.com>
2018-05-31 00:13:28 +0530

    added params

--- pdfDemo.py
+++ pdfDemo.py
@@ -3,11 +3,11 @@
 #    Class for generating demo PDF file           #
 #                                                 #
 ###################################################
-
+import os
 
 class PDFDemo:
     """
     Class for generating PDF file with additional graphical data
     """
     def __init__(self, pdfFileName):
-        pass
+        self.pdfFile = pdfFileName
