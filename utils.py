9255b5222c5c0bd4c99fd336611042c8180d3f8b
luckysher <luckyshera95@gmail.com>
2018-06-01 17:06:40 +0530

    updated method for generating pdf

--- utils.py
+++ utils.py
@@ -2,8 +2,20 @@ import PyPDF2
 import os
 
 def generatePDF(filename):
+    """
+    Method for generating pDF file
+    :param filename:
+    :return:
+    """
     try:
-        print("generating pdf file: '%s'" % filename)
+        print("Generating pdf file: '%s'" % filename)
 
+        # creating a pdf file object
+        pdfFileObj = open(os.path.join(os.getcwd(), 'input/sample.pdf'), 'rb')
+
+        # closing the pdf file object
+        pdfFileObj.close()
     except:
         pass
+
+    print("Generated pdf file: '%s'" % filename)
