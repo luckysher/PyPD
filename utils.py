5bffe14c70d8854ef49471ba939be7011bf499a4
luckysher <luckyshera95@gmail.com>
2018-06-01 17:16:33 +0530

    added method for adding logo in pDf file:

--- utils.py
+++ utils.py
@@ -19,3 +19,19 @@ def generatePDF(filename):
         pass
 
     print("Generated pdf file: '%s'" % filename)
+
+
+def addIconToPdf(iconType='logo'):
+    """
+    Method for adding a icon in the PDF file
+    :param iconType:
+    :return:
+    """
+    if iconType == 'logo':
+        print("adding logo icon to PDF")
+
+    if iconType == 'tick':
+        print("adding tick icon to PDF")
+
+    if iconType == 'cross':
+        print("adding cross icon to PDF")
